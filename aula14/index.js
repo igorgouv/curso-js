24b34802fb145a70d02b8f482ed42d3d7ddab727
igorgouv <igorgouvb@gmail.com>
2021-09-10 14:55:54 -0300

    mudanca

--- aula14/index.js
+++ aula14/index.js
@@ -1,6 +1,6 @@
 // IEEE 754-2008
 let num1 = 0.7; // number
-let num2 = 0.1; // number
+let num2 = 01; // number
 
 // num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.8
 // num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.9
